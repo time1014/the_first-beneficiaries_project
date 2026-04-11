f0d30c15d05da03ea7f4d25ec5436e7d33878428
jinyoung0711 <64236748+time1014@users.noreply.github.com>
2026-04-12 00:30:40 +0900

    Update surBene.js

--- client/src/stores/surBene.js
+++ client/src/stores/surBene.js
@@ -1,29 +1,46 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useBeneStore = defineStore('bene', {
-  state: () => ({
-    beneficiaries_no: 0,
-    manager_no: 0,
-    subManager_no : 0,
-    beneficiaries_name : null,
-    survey_no: 0,
-    refreshCounsel: false,
-    selectedCounsel: null,
-    isEditMode: false
-  }),
-  actions: {
-    async fetchUsers(user_no) {
-      try {
-        const resp = await fetch(`/api/beneficiaries/${user_no}`)
-        const data = await resp.json()
-        this.beneficiaries_no = data[0].beneficiaries_no;
-        this.beneficiaries_name = data[0].beneficiaries_name;
-        this.manager_no = data[0].manager_no;
-        this.subManager_no=  data[0].sub_manager_no;
-        this.survey_no = data[0].survey_no;
-      } catch (err) {
-        console.log(err)
-      }
+export const useUserStore = defineStore('user', () => {
+    const user_no = ref(null)
+    const role = ref('')
+    const user_id = ref('')
+    const user_name = ref('')
+    const approval = ref(0)
+    const institution = ref(0)
+
+    function setUser(user) {
+        user_no.value = user.user_no
+        role.value = user.role
+        user_id.value = user.user_id
+        user_name.value = user.user_name
+        approval.value = user.approval
+        institution.value = user.institution_no
     }
-  }
+
+    function updateUser(data) {
+        user_name.value = data.user_name
+        role.value = data.role
+    }
+
+    function logout() {
+        user_no.value = null
+        role.value = ''
+        user_id.value = ''
+        user_name.value = ''
+        approval.value = 0
+        institution.value = 0
+    }
+
+    return {
+        user_no,
+        role,
+        user_id,
+        user_name,
+        approval,
+        institution,
+        setUser,
+        updateUser,
+        logout
+    }
 })
