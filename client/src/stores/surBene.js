8958e653f728b3944a6539ed33055e404c81e502
jinyoung0711 <64236748+time1014@users.noreply.github.com>
2026-04-12 00:30:55 +0900

    Update surBene.js

--- client/src/stores/surBene.js
+++ client/src/stores/surBene.js
@@ -1,46 +1,41 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useUserStore = defineStore('user', () => {
-    const user_no = ref(null)
-    const role = ref('')
-    const user_id = ref('')
-    const user_name = ref('')
-    const approval = ref(0)
-    const institution = ref(0)
+export const useBeneStore = defineStore('bene', () => {
+  const beneficiaries_no = ref(0)
+  const manager_no = ref(0)
+  const subManager_no = ref(0)
+  const beneficiaries_name = ref(null)
+  const survey_no = ref(0)
 
-    function setUser(user) {
-        user_no.value = user.user_no
-        role.value = user.role
-        user_id.value = user.user_id
-        user_name.value = user.user_name
-        approval.value = user.approval
-        institution.value = user.institution_no
-    }
+  const refreshCounsel = ref(false)
+  const selectedCounsel = ref(null)
+  const isEditMode = ref(false)
 
-    function updateUser(data) {
-        user_name.value = data.user_name
-        role.value = data.role
-    }
+  async function fetchUsers(user_no) {
+    try {
+      const resp = await fetch(`/api/beneficiaries/${user_no}`)
+      const data = await resp.json()
 
-    function logout() {
-        user_no.value = null
-        role.value = ''
-        user_id.value = ''
-        user_name.value = ''
-        approval.value = 0
-        institution.value = 0
+      beneficiaries_no.value = data[0].beneficiaries_no
+      beneficiaries_name.value = data[0].beneficiaries_name
+      manager_no.value = data[0].manager_no
+      subManager_no.value = data[0].sub_manager_no
+      survey_no.value = data[0].survey_no
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    return {
-        user_no,
-        role,
-        user_id,
-        user_name,
-        approval,
-        institution,
-        setUser,
-        updateUser,
-        logout
-    }
+  return {
+    beneficiaries_no,
+    manager_no,
+    subManager_no,
+    beneficiaries_name,
+    survey_no,
+    refreshCounsel,
+    selectedCounsel,
+    isEditMode,
+    fetchUsers
+  }
 })
